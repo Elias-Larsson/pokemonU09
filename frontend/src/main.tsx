3e19cc45044e148d1004e704492182bc7cffeeb1
Elias-Larsson <aroezi05@gmail.com>
2025-05-30 11:30:26 +0200

    feat: add router to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-
+import { Button } from "./components/button.tsx";
+import { App } from "./App.tsx";
+import { RouterProvider } from "react-router";
+import { router } from "./router.tsx";
 createRoot(document.getElementById("root")!).render(
+
   <StrictMode>
+    <Button name="Click Me" color="yellow" />
     <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 );
