83ee4b293dc87a63708a42bb2fc4c89531ca8a13
obeyhades <abdu.nazar145@gmail.com>
2025-06-03 11:37:31 +0200

    refactor: removed unness code

--- frontend/src/pages/splashscreen.tsx
+++ frontend/src/pages/splashscreen.tsx
@@ -1,21 +1,15 @@
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
 export const SplashScreen = () => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate("/login");
-  };
-
   return (
     <>
-      <div
-        onClick={handleClick}
+      <Link
+        to="/login"
         className="w-screen h-screen flex flex-col items-center justify-center text-black text-4xl font-bold space-y-4"
       >
         <img src="/rocketlogo.png" alt="Logo" className="flex w-96 " />
         Press to start
-      </div>
+      </Link>
     </>
   );
 };
