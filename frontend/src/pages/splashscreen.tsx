06f546da5914f40bc63bcc2b21af7e4e10af9036
obeyhades <abdu.nazar145@gmail.com>
2025-06-03 11:30:26 +0200

    feat: add splashScreen

--- frontend/src/pages/splashscreen.tsx
+++ frontend/src/pages/splashscreen.tsx
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router";
+
 export const SplashScreen = () => {
-    return (
-        <>
-        <p>SplashScreen</p>
-        </>
-    )
-}
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <div
+        onClick={handleClick}
+        className="w-screen h-screen flex flex-col items-center justify-center text-black text-4xl font-bold space-y-4"
+      >
+        <img src="/rocketlogo.png" alt="Logo" className="flex w-96 " />
+        Press to start
+      </div>
+    </>
+  );
+};
