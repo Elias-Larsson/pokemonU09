71a7cd97213a1e527acda5986d708a34a80c0c4b
obeyhades <abdu.nazar145@gmail.com>
2025-06-08 15:33:35 +0200

    feat: add profile styling

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,7 +1,16 @@
+import { Button } from "../components/button";
+
 export const Profile = () => {
-    return (
-        <>
-        <p>Profile</p>
-        </>
-    )
-}
+  return (
+    <div className="bg-cover bg-center h-screen flex flex-col items-center justify-center bg-[url('/homebackground.png')]">
+      <h1 className="text-4xl font-pixel text-black mb-4">Profile</h1>
+      <img src="/dragon.svg" alt="Profile Icon" className="w-40 mb-6" />
+      
+      <div className="flex flex-col items-center gap-2">
+        <Button name="Pokemon" color="red" buttonType="link" route="/pokeSearch" />
+        <Button name="Edit" color="white" buttonType="link" route="/editprofile" />
+        <Button name="Log" color="white" buttonType="link" route="/*" />
+      </div>
+    </div>
+  );
+};
