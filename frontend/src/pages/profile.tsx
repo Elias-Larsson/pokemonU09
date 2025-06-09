e2861f78ad700f7cef3f1fb6d2e5d3bf72de864c
obeyhades <abdu.nazar145@gmail.com>
2025-06-09 10:51:21 +0200

    feat: add img & fixed button style

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -6,7 +6,7 @@ export const Profile = () => {
       <h1 className="text-4xl font-pixel text-black mb-4">Profile</h1>
       <img src="/dragon.svg" alt="Profile Icon" className="w-40 mb-6" />
       
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col text-center gap-2 w-24">
         <Button name="Pokemon" color="red" buttonType="link" route="/pokeSearch" />
         <Button name="Edit" color="white" buttonType="link" route="/editprofile" />
         <Button name="Log" color="white" buttonType="link" route="/*" />
