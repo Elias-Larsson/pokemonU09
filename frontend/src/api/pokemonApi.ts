de3e3ddcbc71936418e40ca0f6b877fd7e112d53
obeyhades <abdu.nazar145@gmail.com>
2025-06-10 15:58:23 +0200

    feat: add more pokemon

--- frontend/src/api/pokemonApi.ts
+++ frontend/src/api/pokemonApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const URL = "https://pokeapi.co/api/v2/pokemon";
-export const GET_POKEMONS = `${URL}?limit=150`;
+export const GET_POKEMONS = `${URL}?limit=1350`;
 
 export const getPokemons = (apiURL: string) => {
   return axios.get(apiURL);
