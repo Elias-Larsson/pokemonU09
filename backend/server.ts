02135503e44fa99cda0b0a82b994822eabc95fc2
Elias-Larsson <aroezi05@gmail.com>
2025-06-15 13:44:53 +0200

    refactor: get rid of comments

--- backend/server.ts
+++ backend/server.ts
@@ -44,8 +44,8 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
     cookie: {
       maxAge: 12 * 60 * 60 * 1000,
-      sameSite: "none", // <-- add this
-      secure: true      // <-- add this (required for SameSite=None)
+      sameSite: "none", 
+      secure: true      
     },
   })
 );
