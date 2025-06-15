586defaf33c821d60b54881e5215b603643e8b2b
Elias-Larsson <aroezi05@gmail.com>
2025-06-15 13:44:08 +0200

    fix: allow cookies to be sent cross-site

--- backend/server.ts
+++ backend/server.ts
@@ -44,6 +44,8 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
     cookie: {
       maxAge: 12 * 60 * 60 * 1000,
+      sameSite: "none", // <-- add this
+      secure: true      // <-- add this (required for SameSite=None)
     },
   })
 );
