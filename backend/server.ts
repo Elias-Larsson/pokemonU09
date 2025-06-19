c6f7fe19d78a6acdf29a433d6eeae5f56b829fab
Elias-Larsson <aroezi05@gmail.com>
2025-06-19 17:35:14 +0200

    feat: add cookie configuration for deployment

--- backend/server.ts
+++ backend/server.ts
@@ -40,7 +40,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
-
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      httpOnly: true,
+    }
   })
 );
 
